5b8b9e5a7ac0f51296382d51d40edd1477c667bc
Qizhi Zhang <qzhang@anlvpn001.mcs.anl.gov>
2017-09-22 12:15:52 -0500

    more details on the spec file and remade Impl file, TASK-1082

--- lib/MaSuRCA/MaSuRCAImpl.py
+++ lib/MaSuRCA/MaSuRCAImpl.py
@@ -33,7 +33,7 @@ ftp://ftp.genome.umd.edu/pub/MaSuRCA/latest/
     ######################################### noqa
     VERSION = "0.0.1"
     GIT_URL = "https://github.com/kbaseapps/kb_MaSuRCA.git"
-    GIT_COMMIT_HASH = "c67dbc6062d5cb4673d7a5058e5b6c7aecbfe032"
+    GIT_COMMIT_HASH = "8a2e9c64c9af2e9f4b4c53a4260bd75438c77a02"
 
     #BEGIN_CLASS_HEADER
     # Class variables and functions can be defined in this block
@@ -64,51 +64,57 @@ ftp://ftp.genome.umd.edu/pub/MaSuRCA/latest/
            /FULL_PATH/short_1.fastq  /FULL_PATH/short_2.fastq #pacbio reads
            must be in a single fasta file! make sure you provide absolute
            path PACBIO=/FULL_PATH/pacbio.fa OTHER=/FULL_PATH/file.frg 2.
-           PARAMETERS string GRAPH_KMER_SIZE - the k-mer size for deBruijn
+           PARAMETERS string graph_kmer_size - the k-mer size for deBruijn
            graph values between 25 and 127 are supported, 'auto' will compute
            the optimal size based on the read data and GC content bool
-           USE_LINKING_MATES - set this to 1 for all Illumina-only
+           use_linking_mates - set this to 1 for all Illumina-only
            assemblies; set this to 1 if you have less than 20x long reads
            (454, Sanger, Pacbio) and less than 50x CLONE coverage by
            Illumina, Sanger or 454 mate pairs; otherwise keep at 0 int
-           LIMIT_JUMP_COVERAGE - this parameter is useful if you have too
+           limit_jump_coverage - this parameter is useful if you have too
            many Illumina jumping library mates. Typically set it to 60 for
            bacteria and 300 for the other organisms CA_PARAMETERS: these are
            the additional parameters to Celera Assembler.  do not worry about
            performance, number or processors or batch sizes -- these are
            computed automatically. float cgwErrorRate=0.15 - set
            cgwErrorRate=0.25 for bacteria and 0.1<=cgwErrorRate<=0.15 for
-           other organisms. int KMER_COUNT_THRESHOLD - minimum count k-mers
+           other organisms. int kmer_count_threshold - minimum count k-mers
            used in error correction 1 means all k-mers are used.  one can
-           increase to 2 if Illumina coverage >100 bool CLOSE_GAPS - whether
+           increase to 2 if Illumina coverage >100 bool close_gaps - whether
            to attempt to close gaps in scaffolds with Illumina data (1) or
-           not (0) int NUM_THREADS - auto-detected number of cpus to use,
-           mandatory int JF_SIZE  - this is mandatory jellyfish hash size --
+           not (0) int num_threads - auto-detected number of cpus to use,
+           mandatory int jf_size  - this is mandatory jellyfish hash size --
            a safe value is estimated_genome_size*estimated_coverage (e.g.,
            2000000000) bool SOAP_ASSEMBLY - set this to 1 to use SOAPdenovo
            contigging/scaffolding module.  Assembly will be worse but will
            run faster. Useful for very large (>5Gbp) genomes string
            workspace_name - the name of the workspace from which to take
-           input and store output. int hash_length - an odd integer (if even,
-           it will be decremented) <= 31 string output_contigset_name - the
-           name of the output contigset list<paired_end_lib> read_libraries -
-           Illumina PairedEndLibrary files to assemble min_contig_length -
-           integer to filter out contigs with length < min_contig_length from
-           the MaSuRCA output. Default value is 500 (where 0 implies no
-           filter). @optional min_contig_length @optional cov_cutoff
-           @optional ins_length @optional read_trkg @optional amos_file
-           @optional exp_cov @optional long_cov_cutoff) -> structure:
-           parameter "workspace_name" of String, parameter "hash_length" of
-           Long, parameter "read_libraries" of list of type "read_lib" (The
-           workspace object name of a SingleEndLibrary or PairedEndLibrary
-           file, whether of the KBaseAssembly or KBaseFile type.), parameter
-           "output_contigset_name" of String, parameter "min_contig_length"
-           of Long, parameter "cov_cutoff" of Double, parameter "ins_length"
-           of Long, parameter "read_trkg" of type "bool" (A boolean - 0 for
-           false, 1 for true. @range (0, 1)), parameter "amos_file" of type
+           input and store output. string output_contigset_name - the name of
+           the output contigset list<paired_end_lib> read_libraries -
+           Illumina PairedEndLibrary files to assemble @optional
+           jump_libraries @optional pacbio @optional other_frg_file @optional
+           graph_kmer_size @optional use_linking_mates @optional
+           limit_jump_coverage @optional cgwErrorRate @optional
+           kmer_count_threshold @optional close_gaps @optional soap_assembly)
+           -> structure: parameter "workspace_name" of String, parameter
+           "num_threads" of Long, parameter "jf_size" of Long, parameter
+           "read_libraries" of list of type "read_lib" (The workspace object
+           name of a SingleEndLibrary or PairedEndLibrary file, whether of
+           the KBaseAssembly or KBaseFile type.), parameter "jump_libraries"
+           of list of type "read_lib" (The workspace object name of a
+           SingleEndLibrary or PairedEndLibrary file, whether of the
+           KBaseAssembly or KBaseFile type.), parameter "pacbio" of type
+           "read_lib" (The workspace object name of a SingleEndLibrary or
+           PairedEndLibrary file, whether of the KBaseAssembly or KBaseFile
+           type.), parameter "other_frg_file" of String, parameter
+           "graph_kmer_size" of String, parameter "use_linking_mates" of type
            "bool" (A boolean - 0 for false, 1 for true. @range (0, 1)),
-           parameter "exp_cov" of Double, parameter "long_cov_cutoff" of
-           Double
+           parameter "limit_jump_coverage" of Long, parameter "cgwErrorRate"
+           of Double, parameter "kmer_count_threshold" of Long, parameter
+           "close_gaps" of type "bool" (A boolean - 0 for false, 1 for true.
+           @range (0, 1)), parameter "soap_assembly" of type "bool" (A
+           boolean - 0 for false, 1 for true. @range (0, 1)), parameter
+           "output_contigset_name" of String
         :returns: instance of type "masurcaResults" (Output parameter items
            for run_masurca report_name - the name of the KBaseReport.Report
            workspace object. report_ref - the workspace reference of the
