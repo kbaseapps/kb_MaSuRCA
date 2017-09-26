be0a1943d7423037cfd45632ad30b02654a9de5c
Qizhi Zhang <qzhang@kbasemacpro-qz.gateway.pace.com>
2017-09-25 22:24:10 -0500

    added the runner module, TASK-1082

---
+++ lib/MaSuRCA/core/Program_Runner.py
@@ -0,0 +1,29 @@
+import os.path
+import subprocess
+
+
+class Program_Runner:
+
+    def __init__(self, cmd, scratch_dir):
+        self.scratch_dir = scratch_dir
+        self.executableName = cmd
+
+    def run(self, command, cwd_dir=None):
+        ''' options is an array of command-line parameters passed to the RQCFilter App '''
+        cmmd = command
+
+        if not cwd_dir:
+          cwd_dir = self.scratch_dir
+
+        #print('\nRunning: ' + ' '.join(cmmd))
+        p = subprocess.Popen(cmmd, cwd=cwd_dir, shell=False)
+        exitCode = p.wait()
+
+        if (exitCode == 0):
+            print('\n' + ' '.join(cmmd) + ' was executed successfully, exit code was: ' + str(exitCode))
+        else:
+            raise ValueError('Error running command: ' + ' '.join(cmmd) + '\n' +
+                             'Exit Code: ' + str(exitCode))
+
+        return exitCode
+
