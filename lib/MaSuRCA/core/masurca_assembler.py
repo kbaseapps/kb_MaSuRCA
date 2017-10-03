b84192db0b1c803c8a77c184b5f950ef8642d155
Qizhi Zhang <qzhang@kbasemacpro-qz.gateway.pace.com>
2017-10-03 09:20:02 -0500

    zip only the CA output files, TASK-1082

--- lib/MaSuRCA/core/masurca_assembler.py
+++ lib/MaSuRCA/core/masurca_assembler.py
@@ -109,13 +109,15 @@ class MaSuRCA_Assembler(object):
             "report_name": None
         }
 
-        # 6. save the assembly to KBase if everything has gone well
-        contig_fa_file = 'CA/dedup.genome.scf.fasta'
-        contig_fa_file = os.path.join(self.proj_dir, contig_fa_file)
+        # 6. save the assembly to KBase and, if everything has gone well, create a report
+        ca_dir = os.path.join(self.proj_dir, 'CA')
+        contig_fa_file = 'dedup.genome.scf.fasta'
+        contig_fa_file = os.path.join(ca_dir, contig_fa_file)
         if (assemble_ok == 0 and os.path.isfile(contig_fa_file)):
             self.m_utils.save_assembly(contig_fa_file, wsname, params[self.PARAM_IN_CS_NAME])
-            report_name, report_ref = self.m_utils.generate_report(contig_fa_file, params, self.proj_dir, wsname)
-            returnVal = {'report_name': report_name, 'report_ref': report_ref}
+            if params['create_report'] == 1:
+                report_name, report_ref = self.m_utils.generate_report(contig_fa_file, params, ca_dir, wsname)
+                returnVal = {'report_name': report_name, 'report_ref': report_ref}
         else:
             log("run_assemble process failed.")
 
